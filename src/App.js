09088609bda621de1ac6332e3f1ddd95cec1cde6
H. Anthony Morales <Anthony@admins-MacBook-Air-8.local>
2020-04-09 23:13:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const modalRoot = document.getElementById('modal-root')
+
+class Modal extends React.Component {
+  el = document.createElement('div')
+  componentDidMount() {
+    modalRoot.appendChild(this.el)
+  }
+  componentWillUnmount() {
+    modalRoot.removeChild(this.el)
+  }
+  render() {
+    return ReactDOM.createPortal(
+      <div className='modal-container' onClick={this.props.modal}>
+        <div className='modal-content'>
+          is this a modal?
+        </div>
+      </div>, this.el
+    )
+  }
+}
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { showModal: false }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState({ showModal: !this.state.showModal })
+  }
+
+  render() {
+    return (
+      <div className='before-modal'>
+        <h1 onClick={this.handleClick}>Click Here!</h1>
+        <p>poppin
+to get (something) started
+let's get shit poppin'</p>
+        {this.state.showModal ? <Modal modal={() => this.handleClick()}/> : undefined}
+      </div>
+    )
+  }
 }
 
 export default App;
